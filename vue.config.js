23eacd3c30bb76d039c3a5cf53ec6da1d2a0d227
Ralf Zeiler <rze.entwicklung@gmail.com>
2020-01-27 14:00:34 +0100

    version 1

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  outputDir: "docs",
+  publicPath: '/docs/',
+  pwa: {
+    name: 'App Name',
+    theme_color: "#42b983",
+    background_color: "#42b983",
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    // configure the workbox plugin
+    
+
+  }
+
+};
